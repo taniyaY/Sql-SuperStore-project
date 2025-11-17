a8337b129e97fea0dd0ce8243f1ac167b077db54
taniyaY <taniyakamboj0105@gmail.com>
2025-11-17 14:59:28 +0200

    Create SuperStore database and schemas
    This script creates the 'SuperStore' database and its schemas: 'bronze', 'silver', and 'gold'. It also includes a warning about data loss if the script is executed.

---
+++ datasets/initdatabase.sql
@@ -0,0 +1,36 @@
+/*
+===============================================================
+Create Databases and Schemas
+Script: 
+	The query checks if a database 'SuperStore' exists, if yes,
+	it drops the database and recreate the database. It also creates 
+	schemas: 'bronze', 'silver', 'gold'.
+WARNING: 
+	Running this script will permanentlydrop all the existing data 
+	in the database, so ensure having proper backups before running.
+===============================================================
+*/
+
+-- If exists, drop and recreate database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'SuperStore')
+BEGIN 
+	ALTER DATABASE SuperStore SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE SuperStore;
+END;
+
+-- Create Database 'SuperStore'
+CREATE DATABASE SuperStore;
+
+-- Create Schemas 'Bronze' 
+CREATE SCHEMA bronze;
+GO
+  
+-- Create Schemas 'Silver' 
+CREATE SCHEMA silver;
+GO
+  
+-- Create Schemas 'Gold' 
+CREATE SCHEMA gold;
+GO
+
+
